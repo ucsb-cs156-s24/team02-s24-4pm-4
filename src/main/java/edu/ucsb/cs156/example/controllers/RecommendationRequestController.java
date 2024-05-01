4bfca73f67084b58b9435862136cc19786de01ed
Jaren Lowe <jaren@ucsb.edu>
2024-05-01 14:25:03 -0700

    jl - fixed RecRequest Controller

--- src/main/java/edu/ucsb/cs156/example/controllers/RecommendationRequestController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/RecommendationRequestController.java
@@ -36,7 +36,7 @@ public class RecommendationRequestController extends ApiController {
     @Autowired
     RecommendationRequestRepository recommendationRequestRepository;
 
-    @Operation(summary= "List all Recommendation Requests")
+    @Operation(summary = "List all recommendation requests")
     @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/all")
     public Iterable<RecommendationRequest> allRecommendationRequests() {
@@ -44,79 +44,34 @@ public class RecommendationRequestController extends ApiController {
         return requests;
     }
 
-    @Operation(summary= "Create a new request")
+    @Operation(summary = "Create a new recommendation request")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/post")
-    public RecommendationRequest postRecommendationRequest(
-            @Parameter(name="requesterEmail") @RequestParam String requesterEmail,
-            @Parameter(name="professorEmail") @RequestParam String professorEmail,
-            @Parameter(name="explanation") @RequestParam String explanation,
-            @Parameter(name="date (in iso format, e.g. YYYY-mm-ddTHH:MM:SS; see https://en.wikipedia.org/wiki/ISO_8601)") @RequestParam("dateRequested") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateRequested,
-            @Parameter(name="date (in iso format, e.g. YYYY-mm-ddTHH:MM:SS; see https://en.wikipedia.org/wiki/ISO_8601)") @RequestParam("dateNeeded") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateNeeded,
-            @Parameter(name="done") @RequestParam Boolean done)
+    public RecommendationRequest postRecommendationDate(
+            @Parameter(name = "requesterEmail") @RequestParam String requesterEmail,
+            @Parameter(name = "professorEmail") @RequestParam String professorEmail,
+            @Parameter(name = "explanation") @RequestParam String explanation,
+            @Parameter(name = "dateRequested") @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateRequested,
+            @Parameter(name = "dateNeeded") @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateNeeded,
+            @Parameter(name = "done") @RequestParam boolean done)  
+            
             throws JsonProcessingException {
+        RecommendationRequest requestToPost = new RecommendationRequest();
+        
+        log.info("dateRequested:", dateRequested);
+        log.info("dateNeeded:", dateNeeded);
 
-        // For an explanation of @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-        // See: https://www.baeldung.com/spring-date-parameters
-
-        log.info("dateRequested={}", dateRequested);
-
-        RecommendationRequest recommendationRequest = new RecommendationRequest();
-        recommendationRequest.setRequesterEmail(requesterEmail);
-        recommendationRequest.setProfessorEmail(professorEmail);
-        recommendationRequest.setExplanation(explanation);
-        recommendationRequest.setDateRequested(dateRequested);
-        recommendationRequest.setDateNeeded(dateNeeded);  
-        recommendationRequest.setDone(done);  
-
-        RecommendationRequest savedRecommendationRequest = recommendationRequestRepository.save(recommendationRequest);
-        return savedRecommendationRequest;
-    }
-
-    @Operation(summary= "Get a single request")
-    @PreAuthorize("hasRole('ROLE_USER')")
-    @GetMapping("")
-    public RecommendationRequest getById(
-            @Parameter(name="id") @RequestParam Long id) {
-                RecommendationRequest recommendationRequest = recommendationRequestRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException(RecommendationRequest.class, id));
-
-        return recommendationRequest;
-    }
-
-    @Operation(summary= "Delete a request")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @DeleteMapping("")
-    public Object deleteRecommendationRequest(
-            @Parameter(name="id") @RequestParam Long id) {
-                RecommendationRequest recommendationRequest = recommendationRequestRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException(RecommendationRequest.class, id));
-
-        recommendationRequestRepository.delete(recommendationRequest);
-        return genericMessage("RecommendationRequest with id %s deleted".formatted(id));
-    }
-
-    @Operation(summary= "Update a single request")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @PutMapping("")
-    public RecommendationRequest updateRecommendationRequest(
-            @Parameter(name="id") @RequestParam Long id,
-            @RequestBody @Valid RecommendationRequest incoming) {
-
-                RecommendationRequest recommendationRequest = recommendationRequestRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException(RecommendationRequest.class, id));
+        requestToPost.setRequesterEmail(requesterEmail);
+        requestToPost.setProfessorEmail(professorEmail);
+        requestToPost.setExplanation(explanation);
+        requestToPost.setDateRequested(dateRequested);
+        requestToPost.setDateNeeded(dateNeeded);
+        requestToPost.setDone(done);
 
 
 
-        recommendationRequest.setRequesterEmail(incoming.getRequesterEmail());
-        recommendationRequest.setProfessorEmail(incoming.getProfessorEmail());
-        recommendationRequest.setExplanation(incoming.getExplanation());
-        recommendationRequest.setDateRequested(incoming.getDateRequested());
-        recommendationRequest.setDateNeeded(incoming.getDateNeeded());
-        recommendationRequest.setDone(incoming.getDone());   
+        RecommendationRequest savedRequest = recommendationRequestRepository.save(requestToPost);
 
-        recommendationRequestRepository.save(recommendationRequest);
-
-        return recommendationRequest;
+        return savedRequest;
     }
 }
